113be45330f2de9c32051674d8cd9e0743fdf334
Ben Echols <ben@whynemo.com>
2024-02-06 16:26:54 -0800

    fix types

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -9,7 +9,7 @@ import { Star } from "lucide-react";
 import Image from "next/image";
 import { parseString } from "xml2js";
 
-async function getCurrentBooks() {
+async function getCurrentBooks(): Promise<BookInfo[]> {
   const currentOptions = {
     method: `get`,
     url: `https://www.goodreads.com/review/list`,
@@ -24,7 +24,7 @@ async function getCurrentBooks() {
     },
   };
   const currentShelfListXml = await axios(currentOptions);
-  let currentlyReading;
+  let currentlyReading: BookInfo[] = [];
 
   parseString(currentShelfListXml.data, function (err, result) {
     if (err) {
@@ -38,39 +38,13 @@ async function getCurrentBooks() {
       }
       currentlyReading = result["GoodreadsResponse"]["reviews"][0][
         "review"
+        // @ts-expect-error
       ].map((element) => {
-        var bookElement = element["book"][0];
-
-        var isbnValue = bookElement["isbn"][0];
-        var isbn13Value = bookElement["isbn13"][0];
-        if (isNaN(isbnValue)) {
-          isbnValue = null;
-        }
-        if (isNaN(isbn13Value)) {
-          isbn13Value = null;
-        }
         return {
-          reviewID: element["id"][0],
-          rating: element["rating"][0],
-          dateStarted: element["started_at"][0],
-          dateAdded: element["date_added"][0],
-          dateUpdated: element["date_updated"][0],
-          body: element["body"][0],
-          book: {
-            bookID: bookElement["id"][0]._,
-            isbn: isbnValue,
-            isbn13: isbn13Value,
-            author: bookElement["authors"][0]["author"][0]["name"],
-            textReviewsCount: bookElement["text_reviews_count"][0]._,
-            uri: bookElement["uri"][0],
-            link: bookElement["link"][0],
-            title: bookElement["title"][0],
-            titleWithoutSeries: bookElement["title_without_series"][0],
-            imageUrl: bookElement["image_url"][0],
-            smallImageUrl: bookElement["small_image_url"][0],
-            largeImageUrl: bookElement["large_image_url"][0],
-            description: bookElement["description"][0],
-          },
+          author: element["book"][0]["authors"][0]["author"][0]["name"],
+          link: element["book"][0]["link"][0],
+          title: element["book"][0]["title"][0],
+          imageUrl: element["book"][0]["image_url"][0],
         };
       });
     }
@@ -78,7 +52,7 @@ async function getCurrentBooks() {
   return currentlyReading;
 }
 
-async function getRecentBooks() {
+async function getRecentBooks(): Promise<BookInfo[]> {
   const recentOptions = {
     method: "GET",
     url: "https://www.goodreads.com/review/list",
@@ -93,7 +67,7 @@ async function getRecentBooks() {
     },
   };
   const recentShelfListXml = await axios(recentOptions);
-  let recentlyRead;
+  let recentlyRead: BookInfo[] = [];
 
   parseString(recentShelfListXml.data, function (err, result) {
     if (err) {
@@ -106,40 +80,15 @@ async function getRecentBooks() {
         return; // TODO: handle empty response
       }
       recentlyRead = result["GoodreadsResponse"]["reviews"][0]["review"].map(
+        // @ts-expect-error
         (element) => {
-          var bookElement = element["book"][0];
-
-          var isbnValue = bookElement["isbn"][0];
-          var isbn13Value = bookElement["isbn13"][0];
-          if (isNaN(isbnValue)) {
-            isbnValue = null;
-          }
-          if (isNaN(isbn13Value)) {
-            isbn13Value = null;
-          }
-
           return {
-            reviewID: element["id"][0],
             rating: element["rating"][0],
-            dateStarted: element["started_at"][0],
-            dateAdded: element["date_added"][0],
-            dateUpdated: element["date_updated"][0],
-            body: element["body"][0],
-            book: {
-              bookID: bookElement["id"][0]._,
-              isbn: isbnValue,
-              isbn13: isbn13Value,
-              author: bookElement["authors"][0]["author"][0]["name"],
-              textReviewsCount: bookElement["text_reviews_count"][0]._,
-              uri: bookElement["uri"][0],
-              link: bookElement["link"][0],
-              title: bookElement["title"][0],
-              titleWithoutSeries: bookElement["title_without_series"][0],
-              imageUrl: bookElement["image_url"][0],
-              smallImageUrl: bookElement["small_image_url"][0],
-              largeImageUrl: bookElement["large_image_url"][0],
-              description: bookElement["description"][0],
-            },
+            review: element["body"][0],
+            author: element["book"][0]["authors"][0]["author"][0]["name"],
+            link: element["book"][0]["link"][0],
+            title: element["book"][0]["title"][0],
+            imageUrl: element["book"][0]["image_url"][0],
           };
         }
       );
@@ -148,47 +97,43 @@ async function getRecentBooks() {
   return recentlyRead;
 }
 
-function BookCard({
-  title,
-  author,
-  link,
-  image,
-  rating,
-  review,
-}: {
+type BookInfo = {
   title: string;
   author: string;
   link: string;
-  image: string;
-  rating: number;
-  review: string;
-}) {
+  imageURL: string;
+  rating?: number | undefined;
+  review?: string | undefined;
+};
+
+function BookCard(bookInfo: BookInfo) {
   return (
     <a
-      href={link}
+      href={bookInfo.link}
       target="_blank"
       rel="noreferrer noopener"
       className="max-w-[50%]"
     >
       <Card className="p-4 hover:bg-slate-200 mb-6 max-w-2xl">
-        <CardTitle className="p-2">{title}</CardTitle>
-        <CardDescription className="p-2">{author}</CardDescription>
+        <CardTitle className="p-2">{bookInfo.title}</CardTitle>
+        <CardDescription className="p-2">{bookInfo.author}</CardDescription>
         <CardContent>
           <div className="flex justify-between">
             <div className="flex flex-col w-xl">
-              {rating && (
+              {bookInfo.rating && (
                 <div className="flex">
                   My rating:{" "}
-                  {Array.from({ length: rating }, (_, i) => (
+                  {Array.from({ length: bookInfo.rating }, (_, i) => (
                     <Star key={i} />
                   ))}
                 </div>
               )}
-              {review && <div className="pt-4 max-w-[80%]">{review}</div>}
-              {!rating && !review && <div>Currently reading</div>}
-            </div>
-            <div className="shrink-0">
-              <Image src={image} alt="Book cover" width={50} height={200} />
+              {bookInfo.review && (
+                <div className="pt-4 max-w-[80%]">{bookInfo.review}</div>
+              )}
+              {!bookInfo.rating && !bookInfo.review && (
+                <div>Currently reading</div>
+              )}
             </div>
           </div>
         </CardContent>
@@ -198,30 +143,30 @@ function BookCard({
 }
 
 export default async function Books() {
-  const currentBooks: Promise<string> = await getCurrentBooks();
+  const currentBooks = await getCurrentBooks();
   const recentBooks = await getRecentBooks();
   return (
     <div>
       <h1 className="text-2xl font-bold pb-4">Currently reading</h1>
-      {currentBooks.map((review) => (
+      {currentBooks.map((bookInfo) => (
         <BookCard
-          title={review.book.title}
-          author={review.book.author}
-          key={review.book.title}
-          link={review.book.link}
-          image={review.book.imageUrl}
+          title={bookInfo.title}
+          author={bookInfo.author}
+          key={bookInfo.title}
+          link={bookInfo.link}
+          imageURL={bookInfo.imageURL}
         />
       ))}
       <h1 className="text-2xl font-bold py-4">Recently read</h1>
-      {recentBooks.map((review) => (
+      {recentBooks.map((bookInfo) => (
         <BookCard
-          title={review.book.title}
-          author={review.book.author}
-          key={review.book.title}
-          link={review.book.link}
-          image={review.book.imageUrl}
-          rating={review.rating}
-          review={review.body}
+          title={bookInfo.title}
+          author={bookInfo.author}
+          key={bookInfo.title}
+          link={bookInfo.link}
+          imageURL={bookInfo.imageURL}
+          rating={bookInfo.rating}
+          review={bookInfo.review}
         />
       ))}
     </div>
